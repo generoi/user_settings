6b837a05be6cd00f0164c31d39f92b5f99d1ac4b
Oskar Schöldström <public@oxy.fi>
2014-04-02 07:07:42 +0200

    bring in changes done on woolspire

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,22 @@
     var f = $.farbtastic('#color-picker');
     var selected;
     $('.color')
-      .each(function() { f.linkTo(this); })
+      .each(function() {
+        f.linkTo(this);
+      })
       .focus(function() {
         if (selected) {
-          $(selected).removeClass('color-selected');
+          $(selected).parent('.form-item').removeClass('color-selected');
         }
         f.linkTo(this);
-        $(selected = this).addClass('color-selected');
+        $(selected = this).parent('.form-item').addClass('color-selected');
       });
 
+    $('.select-font input')
+      .each(function() {
+        var t = $(this);
+        t.siblings('label').css('font-family', t.val());
+      });
   });
 
 }(jQuery));
